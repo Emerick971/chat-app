be3b0bf30d719e00ba14e91464686b4aa093afce
Emerick971 <emerickmartial@gmail.com>
2020-12-11 12:43:11 +0100

    New branch and Contact List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+      avatar="img random.jpg"
+      name="Anita Sutton"
+      online = {true}
+      />
+      <Contact 
+      avatar="kirk.jpg"
+      name="Kirk Fox"
+      online = {false}
+      />
+      <Contact 
+      avatar="herminia.jpg"
+      name="Herminia Brown"
+      online = {true}
+      />
+      <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-function Contact() {
+function Contact(props) {
     return (
       <div className="Contact">
           
-            <img className="avatar" src="img random.jpg" alt="random" />
+            <img className="avatar" src={props.avatar} alt={props.name} />
           <div className="status">
-              <h3 className="name">Anita Sutton</h3>
-              <div className="status-online"></div>
-              <p className="status-text">online</p>
+              <h3 className="name">{props.name}</h3>
+              <div className={props.online ? 'status-online' : 'status-offline'}></div>
+              <p className="status-text">{props.online ? 'online' : 'offline'}</p>
           </div>
       </div>
     );
   }
+
+  Contact.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired
+
+  };
   
   export default Contact;
